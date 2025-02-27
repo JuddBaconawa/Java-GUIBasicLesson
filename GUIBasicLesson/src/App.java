e860b0f3c7597eee0f8cc8cbf525936d3c02ee39
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-02-27 11:47:01 -0500

    Project created on VisualStudio + initial class created
    App.java is organized and commented for readability

---
+++ GUIBasicLesson/src/App.java
@@ -0,0 +1,15 @@
+//packages
+
+
+//imports
+
+//main class - App
+public class App {
+
+    //main method where most of the codes will be executed
+    public static void main(String[] args) throws Exception {
+       
+        
+
+    }
+}
